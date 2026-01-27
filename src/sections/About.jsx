454094944d80392bd99712b6aceaaac9227b52b2
guiss26 <guissella584@gmail.com>
2026-01-27 14:02:43 -0800

    fix(about): correct class name

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,10 +2,10 @@
 export default function About() {
     return (
         <>
-            <section id="about" className="min-h-screen text-left bg-gray-100/100 flex flex-col justify-center">
+            <section id="about" className="min-h-screen text-left bg-gray-100 flex flex-col justify-center">
                 <div className="max-w-5xl mx-auto flex flex-col gap-3">
-                    <h1 className="text-6xl">Sobre mí</h1>
-                    <h2 className="text-2xl bg-linear-to-r from-teal-600 to-blue-900 bg-clip-text text-transparent">Desarrolladora Full-Stack Junior</h2>
+                    <h2 className="text-6xl">Sobre mí</h2>
+                    <h3 className="text-2xl bg-linear-to-r from-teal-600 to-blue-900 bg-clip-text text-transparent">Desarrolladora Full-Stack Junior</h3>
                     <p className="mt-3 text-neutral-700">Recién graduada de un bootcamp intensivo de Desarrollo Web Full-Stack y titulada en Desarrollo de Aplicaciones Multiplataforma. Apasionada por el
                         <span className="font-bold"> frontend</span>, donde combino creatividad, lógica y atención al detalle para construir interfaces funcionales y centradas en el usuario. Trabajo con <span className="font-bold">React, JavaScript, Tailwind CSS y MUI</span>, y tengo experiencia en <span className="font-bold">Node.js, Express, MongoDB y MySQL</span> en proyectos académicos.</p>
                     <p className="text-neutral-700">Disfruto <span className="font-bold">aprender, resolver desafíos y trabajar en equipo</span>, y busco oportunidades donde aportar mis conocimientos, seguir creciendo y participar en proyectos que valoren la mejora continua y la colaboración.</p>
