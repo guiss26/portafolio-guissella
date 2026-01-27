0a102b4f8d7ec163867548035781475f5b62f8ed
guiss26 <guissella584@gmail.com>
2026-01-27 10:12:02 -0800

    style(navbar: update brand name to guiperez

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
         <header className="bg-teal-50 flex p-4">
             <nav className="flex justify-between w-full">
                 <div>
-                    <h3 className="text-xl font-semibold bg-gradient-to-r from-teal-600 to-blue-900 bg-clip-text text-transparent">guissella_dev</h3>
+                    <h3 className="text-xl font-semibold bg-gradient-to-r from-teal-600 to-blue-900 bg-clip-text text-transparent">guiperez_dev</h3>
                 </div>
                 <div>
                     <ul className="flex gap-10">
