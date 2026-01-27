8a205bbb6f85b1e13e831daac555c51dcaf38e62
guiss26 <guissella584@gmail.com>
2026-01-27 12:08:50 -0800

    style(ui): update font weight and size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,20 @@
 export default function Navbar() {
     return (
         <>
-        <header className="bg-teal-50 flex p-4">
+        <header className="fixed top-0 left-0 right-0 z-50 bg-white/80 backdrop-blur-md shadow-sm flex py-4 px-8">
             <nav className="flex justify-between w-full">
                 <div>
-                    <h3 className="text-xl font-semibold bg-gradient-to-r from-teal-600 to-blue-900 bg-clip-text text-transparent">guiperez_dev</h3>
+                    <h3 className="text-xl font-semibold bg-linear-to-r from-teal-600 to-blue-900 bg-clip-text text-transparent">guiperez_dev</h3>
                 </div>
                 <div>
                     <ul className="flex gap-10">
-                        <li className="text-neutral-600 hover:text-teal-700 transition-colors">Proyectos</li>
-                        <li className="text-neutral-600 hover:text-teal-700 transition-colors">Habilidades</li>
-                        <li className="text-neutral-600 hover:text-teal-700 transition-colors">Contacto</li>
+                        <li className="text-neutral-600 hover:text-teal-700 transition-colors font-medium">Proyectos</li>
+                        <li className="text-neutral-600 hover:text-teal-700 transition-colors font-medium">Habilidades</li>
+                        <li className="text-neutral-600 hover:text-teal-700 transition-colors font-medium">Contacto</li>
                     </ul>
                 </div>
+
+                {/* añadir modo oscuro */}
             </nav>
         </header>
         </>
