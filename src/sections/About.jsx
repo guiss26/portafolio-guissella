35d5c61c2302b57e7e59c47c5a5b1bf1494a7fed
guiss26 <guissella584@gmail.com>
2026-01-27 13:49:47 -0800

    style(about): update paragraph text color

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,9 +6,9 @@ export default function About() {
                 <div className="max-w-5xl mx-auto flex flex-col gap-3">
                     <h1 className="text-6xl">Sobre mí</h1>
                     <h2 className="text-2xl bg-linear-to-r from-teal-600 to-blue-900 bg-clip-text text-transparent">Desarrolladora Full-Stack Junior</h2>
-                    <p className="mt-3">Recién graduada de un bootcamp intensivo de Desarrollo Web Full-Stack y titulada en Desarrollo de Aplicaciones Multiplataforma. Apasionada por el
+                    <p className="mt-3 text-neutral-700">Recién graduada de un bootcamp intensivo de Desarrollo Web Full-Stack y titulada en Desarrollo de Aplicaciones Multiplataforma. Apasionada por el
                         <span className="font-bold"> frontend</span>, donde combino creatividad, lógica y atención al detalle para construir interfaces funcionales y centradas en el usuario. Trabajo con <span className="font-bold">React, JavaScript, Tailwind CSS y MUI</span>, y tengo experiencia en <span className="font-bold">Node.js, Express, MongoDB y MySQL</span> en proyectos académicos.</p>
-                    <p>Disfruto <span className="font-bold">aprender, resolver desafíos y trabajar en equipo</span>, y busco oportunidades donde aportar mis conocimientos, seguir creciendo y participar en proyectos que valoren la mejora continua y la colaboración.</p>
+                    <p className="text-neutral-700">Disfruto <span className="font-bold">aprender, resolver desafíos y trabajar en equipo</span>, y busco oportunidades donde aportar mis conocimientos, seguir creciendo y participar en proyectos que valoren la mejora continua y la colaboración.</p>
 
                     {/* mejorar el segundo párrafo */}
                 </div>
