59c0e568a8cc0e215e9db27da3f7df4dc130c064
guiss26 <guissella584@gmail.com>
2026-01-27 10:07:40 -0800

    feat(navbar): add initial navbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,20 @@
 export default function Navbar() {
     return (
         <>
-        Este es el Navbar
+        <header className="bg-teal-50 flex p-4">
+            <nav className="flex justify-between w-full">
+                <div>
+                    <h3 className="text-xl font-semibold bg-gradient-to-r from-teal-600 to-blue-900 bg-clip-text text-transparent">guissella_dev</h3>
+                </div>
+                <div>
+                    <ul className="flex gap-10">
+                        <li className="text-neutral-600 hover:text-teal-700 transition-colors">Proyectos</li>
+                        <li className="text-neutral-600 hover:text-teal-700 transition-colors">Habilidades</li>
+                        <li className="text-neutral-600 hover:text-teal-700 transition-colors">Contacto</li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
         </>
     )
 }
