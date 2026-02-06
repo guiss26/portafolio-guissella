ff4b186c665c92a31911c2f5b8219ea211ae0bd9
guiss26 <guissella584@gmail.com>
2026-02-06 10:18:05 -0800

    style(skills): set padding size

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -12,7 +12,7 @@ const skillsCategories = [ //probar hacerlo con un array de objeto y recorrerlo
 export default function Skills() {
     return (
         <>
-            <section id="skills" className='min-h-screen bg-neutral-50 flex flex-col justify-center py-20'>
+            <section id="skills" className='min-h-screen bg-neutral-50 flex flex-col justify-center py-15'>
                 <article className='mx-8'>
                     <div>
                         <h2 className='text-5xl pb-2.5'>Habilidades</h2>
@@ -22,7 +22,7 @@ export default function Skills() {
                     {/* skills-box */}
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-10'>
                         {/* frontend */}
-                        <div id="frontend" className='bg-white hover:shadow-xl transition-all duration-300 p-6 rounded-xl'>
+                        <div id="frontend" className='bg-white hover:shadow-xl transition-all duration-300 p-5 rounded-xl'>
                             <div className='flex gap-2.5 items-center'>
                                 <div className='bg-linear-to-r from-violet-600 to-indigo-600 rounded-xl p-2'>
                                     <Code2 className='size-6 text-white' />
@@ -43,7 +43,7 @@ export default function Skills() {
                         </div>
 
                         {/* backend  */}
-                        <div id="backend" className='bg-white hover:shadow-xl transition-all duration-300 p-6 rounded-xl'>
+                        <div id="backend" className='bg-white hover:shadow-xl transition-all duration-300 p-5 rounded-xl'>
                             <div className='flex gap-2.5 items-center'>
                                 <div className='bg-linear-to-r from-emerald-600 to-teal-600 rounded-xl p-2'>
                                     <DatabaseZap className='size-6 text-white' />
@@ -61,7 +61,7 @@ export default function Skills() {
                         </div>
 
                         {/* databases */}
-                        <div id="bbdd" className='bg-white hover:shadow-xl transition-all duration-300 p-6 rounded-xl'>
+                        <div id="bbdd" className='bg-white hover:shadow-xl transition-all duration-300 p-5 rounded-xl'>
                             <div className='flex gap-2.5 items-center'>
                                 <div className='bg-linear-to-r from-pink-600 to-rose-600 rounded-xl p-2'>
                                     <Database className='size-6 text-white' />
@@ -75,7 +75,7 @@ export default function Skills() {
                         </div>
 
                         {/* testing */}
-                        <div id="testing" className='bg-white hover:shadow-xl transition-all duration-300 p-6 rounded-xl'>
+                        <div id="testing" className='bg-white hover:shadow-xl transition-all duration-300 p-5 rounded-xl'>
                             <div className='flex gap-2.5 items-center'>
                                 <div className='bg-linear-to-r from-orange-600 to-amber-600 rounded-xl p-2'>
                                     <FileCog className='size-6 text-white'/>
@@ -91,7 +91,7 @@ export default function Skills() {
                         </div>
 
                         {/* security */}
-                        <div id="seguridad" className='bg-white hover:shadow-xl transition-all duration-300 p-6 rounded-xl'>
+                        <div id="seguridad" className='bg-white hover:shadow-xl transition-all duration-300 p-5 rounded-xl'>
                             <div className='flex gap-2.5 items-center'>
                                 <div className='bg-linear-to-r from-cyan-600 to-blue-600 rounded-xl p-2'>
                                     <ShieldCheck className='size-6 text-white'/>
@@ -105,7 +105,7 @@ export default function Skills() {
                         </div>
 
                         {/* tools */}
-                        <div id="herramientas" className='bg-white hover:shadow-xl transition-all duration-300 p-6 rounded-xl'>
+                        <div id="herramientas" className='bg-white hover:shadow-xl transition-all duration-300 p-5 rounded-xl'>
                             <div className='flex gap-2.5 items-center'>
                                 <div className='bg-linear-to-r from-purple-600 to-violet-600 rounded-xl p-2'>
                                     <Zap className='size-6 text-white'/>
