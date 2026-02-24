342ceef8b7d7a492bf9050a74206bf2b9cef0da7
guiss26 <guissella584@gmail.com>
2026-02-24 10:37:05 -0800

    fix(pagination): remove invalid props from Lucide icon components in Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,7 +2,7 @@ import { ChevronLeft } from 'lucide-react';
 import { ChevronRight } from 'lucide-react';
 import { useState } from 'react';
 
-export default function Pagination({ page, setPage, max}) {
+export default function Pagination({ page, setPage, max }) {
     const [spanPage, setSpanPage] = useState(1)
 
     const nextPage = () => {
@@ -22,9 +22,9 @@ export default function Pagination({ page, setPage, max}) {
     return (
         <>
             <div className='flex items-center justify-center gap-3 '>
-                <button onClick={previousPage} disabled={page <= 1} className='cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed disabled:opacity-50 transition-all'><ChevronLeft page={page} /></button>
+                <button onClick={previousPage} disabled={page <= 1} className='cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed disabled:opacity-50 transition-all'><ChevronLeft /></button>
                 <p><span className='font-bold'>{spanPage}</span> de <span className='font-bold'>{max}</span> </p>
-                <button onClick={nextPage} disabled={page >= max} className='cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed disabled:opacity-50 transition-all'><ChevronRight setPage={setPage} /></button>
+                <button onClick={nextPage} disabled={page >= max} className='cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed disabled:opacity-50 transition-all'><ChevronRight /></button>
             </div>
         </>
     )
